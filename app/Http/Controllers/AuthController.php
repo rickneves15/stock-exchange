95611bdec6b1eb862a002249b8a23bbcca14e07b
Richard Neves <ricknevesbc@icloud.com>
2023-03-08 17:33:39 -0300

    FEAT: added register, autenticate and logout user

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use App\Traits\ResponseTrait;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    use ResponseTrait;
+
+    public function register(RegisterRequest $request)
+    {
+        try {
+            $validation = $request->validated();
+            $user = new User([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $user->save();
+
+            return $this->responseSuccess($user, 'Successfully registered !');
+        } catch (\Exception $e) {
+            return $this->responseError(null, $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function login(LoginRequest $request)
+    {
+        try {
+            $validation = $request->validated();
+            $credentials = $request->only(['email', 'password']);
+
+            if (!Auth::attempt($credentials)) {
+                return response()->json([
+                    'message' => 'Invalid email or password',
+                ], 401);
+            }
+
+            $user = $request->user();
+
+            $token = $user->createToken('auth-token')->plainTextToken;
+
+            $data = [
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+            ];
+
+            return $this->responseSuccess($data, 'Login successfully !');
+        } catch (\Exception $e) {
+            return $this->responseError(null, $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function logout()
+    {
+
+        try {
+            auth()->user()->tokens()->delete();
+
+            return $this->responseSuccess([], 'Logout successfully !');
+        } catch (\Exception $e) {
+            return $this->responseError(null, $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
